0510de78b04417a6a27b61fb308fd620d10f5b1f
Aldhair Vera <aldhair@subterra.ai>
2022-10-18 23:44:17 -0500

    Layout form for login

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 //Components
-import { Text } from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { LoginForm, UserData } from "../components/Auth/";
 
 //Styles
-import { styles } from '../styles/';
+// import { styles } from "../styles/";
 
 export default function Account() {
+  const auth = null;
+
   return (
-    <SafeAreaView style={styles.container}>
-        <StatusBar style="auto" />
-        <Text>Account</Text>
+    <SafeAreaView>
+      <StatusBar style="auto" />
+      {auth ? <UserData /> : <LoginForm />}
     </SafeAreaView>
-  )
+  );
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "aldhaV15",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "aldhaV15",
+  firstName: "Aldhair",
+  lastName: "Vera Camacho",
+  email: "aldhairvera15@",
+};
